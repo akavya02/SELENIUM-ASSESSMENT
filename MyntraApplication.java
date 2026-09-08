8c1fac748beaaa763d07eaaed961d734da1347ea
akavya02 <167055362+akavya02@users.noreply.github.com>
2026-09-08 17:43:48 +0530

    Refactor main method for product wishlist functionality
    Updated the main method to improve product selection and interaction. Changed implicit wait duration and refined XPath selectors.

--- MyntraApplication.java
+++ MyntraApplication.java
@@ -1,33 +1,28 @@
 package assessmentDAY1;
 
 import java.time.Duration;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class MyntraApplication {
-	public static void main(String[] args) throws InterruptedException {
-		 WebDriver driver=new ChromeDriver();
-			
-			driver.manage().window().maximize();
-			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
-			driver.get("https://www.myntra.com/");
-	        driver.findElement(By.xpath("//input[@class='desktop-searchBar']")).sendKeys("dresses");
-	        driver.findElement(By.xpath("//a[@class='desktop-submit']")).click();
-	        driver.findElement(By.xpath("//div[@class='product-imageSliderContainer']"));
-	        Thread.sleep(5000);
-	        driver.findElement(By.xpath("//span[@class='product-wishlistFlex product-actionsButton product-wishlist ']")).click();
-	        String text=driver.getTitle();
-	        if(text.equals("Myntra")) {
-	        	System.out.println("Product added to wishlist");
-	        }
-	        Thread.sleep(2000);
-	        driver.quit();
-		
-	}
 
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.get("https://www.myntra.com/");
+        driver.findElement(By.xpath("//input[contains(@class,'desktop-searchBar')]")).sendKeys("dresses");
+        driver.findElement(By.xpath("//a[contains(@class,'desktop-submit')]")).click();
+        Thread.sleep(5000);
+        WebElement product = driver.findElement(By.xpath("(//li[contains(@class,'product-base')])[1]"));
+        new Actions(driver).moveToElement(product).perform();
+        driver.findElement(By.xpath("(//li[contains(@class,'product-base')])[1]//span[contains(@class,'product-wishlist')]")).click();
+        System.out.println("Product added to wishlist");
+        driver.quit();
+    }
 }
-
-
-
