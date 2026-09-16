c136b1fba823cb975db6dcd34aaa69ad2da1fb95
akavya02 <167055362+akavya02@users.noreply.github.com>
2026-09-16 09:36:42 +0530

    Add OrangeLoginPage class for login functionality

---
+++ DAY8/OrangeLoginPage.java
@@ -0,0 +1,48 @@
+package DAY8;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class OrangeLoginPage {
+
+    WebDriver driver;
+
+    // Constructor
+    public OrangeLoginPage(WebDriver driver) {
+
+        this.driver = driver;
+
+        PageFactory.initElements(driver, this);
+    }
+
+    // Locators
+
+    @FindBy(name = "username")
+    private WebElement usernameTextField;
+
+    @FindBy(name = "password")
+    private WebElement passwordTextField;
+
+    @FindBy(xpath = "//button[@type='submit']")
+    private WebElement loginButton;
+
+
+    // Actions
+
+    public void enterUsername(String username) {
+
+        usernameTextField.sendKeys(username);
+    }
+
+    public void enterPassword(String password) {
+
+        passwordTextField.sendKeys(password);
+    }
+
+    public void clickLogin() {
+
+        loginButton.click();
+    }
+}
